58f6a0841bb7cf923c7d7a1ab2006699715c8528
Vidya Muniraju <vidya.muniraju@csu.fullerton.edu>
2020-10-02 18:32:05 -0500

    Update scripts.js
    Created a new list for the pokemons and added each pokemon as an object with properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favoriteFood = 'Pasta with Pesto Sauce';
-document.write(favoriteFood);
+pokemonList.push(
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    ability: ['chlorophyll', 'overgrow']
+  }
+);
+
+pokemonList.push(
+  {
+    name: "Rattata",
+    height: 0.3,
+    ability: ['runaway', 'hustle', 'guts']
+  }
+);
+
+pokemonList.push(
+  {
+    name: "Pikachu",
+    height: 0.4,
+    ability: ['static', 'lightningrod']
+  }
+);
+
+pokemonList.push(
+  {
+    name: "Butterfree",
+    height: 1.1,
+    ability: ['compoundeyes', 'tintedlens']
+  }
+);
+
+// console.log(pokemonList[1]);
+// document.write(pokemonList[1].height);
